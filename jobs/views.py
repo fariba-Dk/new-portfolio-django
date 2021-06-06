97f58d2f767b0d0a51f1ddd1df468f09aef96c00
Fariba DK <faribacodes@gmail.com>
2021-06-06 16:48:23 -0700

    added blog

--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-
-
 from django.http import HttpResponse
 
 
-#views methods always take req as first arg
 def home(request):
-    return HttpResponse("Hello World!")
+    return HttpResponse('home.html')
