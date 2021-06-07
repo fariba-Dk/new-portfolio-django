d6550d8551f711a4a5e3d15f37df021893250af7
Fariba DK <faribacodes@gmail.com>
2021-06-06 19:07:37 -0700

    allblogs html addes

--- blog/urls.py
+++ blog/urls.py
@@ -15,15 +15,16 @@ Including another URLconf
 """
 
 from django.urls import path
-from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
-from django.conf.urls import include, url
-import jobs.views
+# from django.contrib import admin
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf.urls import include, url
+from . import views
 # from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home'),
-    path('blog/', include('blog.url'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # path('admin/', admin.site.urls),
+    path('', views.allblogs, name='allblogs'),
+    # path('blog/', include('blog.url'))
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- blog/views.py
+++ blog/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from . import views
+
+def allblogs(request):
+  return render(request, 'blog/allblogs.html')
