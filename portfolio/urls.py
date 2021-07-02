d04f420a51fb8ce3ac2137a138589f6e32828401
Fariba DK <faribacodes@gmail.com>
2021-07-02 10:32:57 -0700

    added pillow

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import include, url
 import jobs.views
+# from django.conf.urls import include, url
 # from django.conf.urls import url
 
 
